d9b24ead17af2cd5ec62ac40c217186ada3e3f50
Glitch (cubic-viscose) <none>
2019-07-15 15:16:06 +0000

    🏄🌓 Checkpoint ./mongoose.js:383815/365 ./server.js:383815/2838

--- mongoose.js
+++ mongoose.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-if (process.env.NODE_ENV === 'test') {
-  mongoose.connect(`mongodb://localhost:27017/personalLibraryTest`, {
-    useNewUrlParser: true
-  });
-} else {
-  const mongoDB = `mongodb://${process.env.DB_USER}:${
-    process.env.DB_PASS
-  }@ds139295.mlab.com:39295/personal-library`;
+
+  const mongoDB = `mongodb://<dbuser>:<dbpassword>@ds151876.mlab.com:51876/ffc-personal-library`;
   mongoose.connect(mongoDB, {
     useNewUrlParser: true
   });
@@ -16,7 +10,7 @@ if (process.env.NODE_ENV === 'test') {
   const db = mongoose.connection;
 
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-}
+
 
 module.exports = {
   mongoose
